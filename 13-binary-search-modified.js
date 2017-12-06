f1349cb2ad70e679856d02ef10c04781dd942e8b
Jon Roach <roach.jc@gmail.com>
2017-12-06 09:24:14 -0800

    13

--- 13-binary-search-modified.js
+++ 13-binary-search-modified.js
@@ -6,21 +6,27 @@
  * @returns {integer} index where array rotates
  */
 function findRotationPointInSemiSorted(array) {
-  let floorIndex = 1;
+  // array is not rotated so just return first elem
+  if (array[0] < array[array.length - 1]) return 0;
+
+  let floorIndex = 0;
   let ceilingIndex = array.length - 1;
   const firstEl = array[0];
 
   while (ceilingIndex > floorIndex) {
     const guessIndex = Math.floor(((ceilingIndex - floorIndex) / 2) + floorIndex);
+
+    // we know that if the guess is greater than the first element
+    // then the start (the smallest element) must be to the right
     if (array[guessIndex] > firstEl) {
       floorIndex = guessIndex;
     }
     if (array[guessIndex] < firstEl) {
       ceilingIndex = guessIndex;
     }
-
     /**
-     * This is the key peice. Without it, the loop continues endlessly with guess = floor = ceiling - 1
+     * This is the key peice. Without it, the loop continues endlessly...
+     * guess === floor === ceiling - 1
      * But when the floor and ceiling are adjacent we know that
      * the ceiling is the start and the floor is the end.
      */
@@ -29,7 +35,7 @@ function findRotationPointInSemiSorted(array) {
   return ceilingIndex;
 }
 
-const arr = ['k', 'v', 'a', 'b', 'c', 'd', 'e', 'g', 'i'];
+const arr = ['b', 'c', 'd', 'e', 'g', 'i', 'a'];
 
 console.log(findRotationPointInSemiSorted(arr));
 
